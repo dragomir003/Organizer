d8c4c96f6db8b267090d1be5aaa034fb0127b0aa
Dragon987 <dragolekovic@gmail.com>
2022-04-08 22:34:46 +0200

    Created Login Form and connected succesfully to database

--- Organizer/Login.Designer.cs
+++ Organizer/Login.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Organizer
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -32,10 +32,9 @@ namespace Organizer
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Login";
         }
 
         #endregion
     }
 }
-
---
+++ Organizer/Login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Organizer
+{
+    public partial class Login : Form
+    {
+        SqlConnection conn;
+        public Login()
+        {
+            InitializeComponent();
+
+            conn = new SqlConnection(ConfigurationManager.AppSettings["dbConnString"]);
+
+            conn.Open();
+
+            MessageBox.Show(conn.State == ConnectionState.Open ? "Allright" : "Not allright");
+
+            conn.Close();
+        }
+
+
+    }
+}
